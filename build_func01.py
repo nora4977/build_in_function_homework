a99d33f1a2d463a2bb29f39306be0b6ae1be93a4
nora4977 <nora4977@nodirayoqubjnova9@gmail.com>
2022-12-06 23:01:43 +0500

    01 donee

--- build_func01.py
+++ build_func01.py
@@ -1,10 +1,11 @@
 def main(number):
     """A integer type variable 'number' is given. Return the absolute value of a "number". 
-
+number=-8
     Args:
         number (int): integer
-
+abs(-8)
     Returns:
         int: the absolute value of a "number"
     """
     return
+print(abs(-8))
